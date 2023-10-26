e72abc629cfb86b774cfd9807608529becde4d7c
Temirlan504 <113613569+Temirlan504@users.noreply.github.com>
2023-10-26 19:24:58 -0400

    Added styling
    Added some styling on the page.
    
    Added progress bar.

--- static/js/unit_detailed.js
+++ static/js/unit_detailed.js
@@ -1,27 +1,53 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // On initial load, display the first section
-    const sections = document.querySelectorAll('.section');
-
-    if (sections.length > 0) {
-        sections[0].style.display = 'block';
-    }
-
-    function showDiv() {
-        for (let i = 0; i < sections.length; i++) {
-            if (sections[i].style.display === "none") {
-                sections[i].style.display = "block";
-                if (i === sections.length - 1) {
-                    showButton.style.display = "none";
-                }
-                sections[i].scrollIntoView({ behavior: "smooth" });
-                return;
-            }
+    // Adjust image layout based on the width of the image
+    function adjustImageLayout(imgElement) {
+        const maxWidthInline = 250; // Change this value based on your preference
+        if (imgElement.width > maxWidthInline) {
+            imgElement.parentElement.classList.add('image-block');
+        } else {
+            imgElement.parentElement.classList.add('image-inline');
         }
     }
 
-    // Add an event listener for the button
+    const sections = document.querySelectorAll('.section');
+    const progressBar = document.getElementById('myBar');
     const showButton = document.getElementById('show-div');
+    let currentSection = 0;
+
+    if (sections.length > 0) {
+        sections[0].style.display = 'block';
+        currentSection = 1;
+        updateProgressBar();
+    }
+
+    function updateProgressBar() {
+        const progressPercentage = (currentSection / sections.length) * 100;
+        progressBar.style.width = progressPercentage + "%";
+    }
+
+    function showDiv() {
+        if (currentSection < sections.length) {
+            sections[currentSection].style.display = "block";
+            sections[currentSection].scrollIntoView({ behavior: "smooth" });
+            currentSection++;
+            updateProgressBar();
+        }
+    
+        if (currentSection === sections.length) {
+            showButton.style.display = "none";
+        }
+    }
+    
+
+    // Add an event listener for the button
     if (showButton) {
         showButton.addEventListener('click', showDiv);
     }
+
+    let images = document.querySelectorAll('.section-image');
+    images.forEach(img => {
+        img.onload = function() {
+            adjustImageLayout(img);
+        };
+    });
 });
